1ebb1a91b280986c6fad6904bd94a447c72ec3ba
Silent Dev <rez_a@outlook.com>
2019-04-06 13:20:43 -0400

    All 4 of the buttons work and show content.

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+
+$('#btn1').on("click", function () {
+    $('#body').html('<style> #container{display: none;} </style>');
+    $('#body').text('As a student in the field of full stack development, I am an individual who is compassionate in building websites and programming them. Other qualifications include an advanced diploma in Electrical Engineering Technology : Control Systems from Humber College and I also worked in construction.');
+    $('#header').html("About Me <br> <br>");
+});
+
+$('#btn2').on("click", function () {
+    $('#body').html('<style> #container{display: none;} </style>');
+    $('#body').text('I have learned and earned experience in html, css, javascript, jQuery, bootstrap, AJAX, JSON and with APIs. I have also create a project with my group to track interesting events in any cities that have events. This is available to be viewed down below in the project section.');
+    $('#header').html(" My Experience <br> <br>")
+});
+
+$('#btn3').on("click", function () {
+    $('#body').html('<style> #container{display: none;} </style>');
+    $('#body').text('I have learned and earned experience in html, css, javascript, jQuery, bootstrap, AJAX, JSON and with APIs. I have also create a project with my group to track interesting events in any cities that have events. This is available to be viewed down below in the project section.');
+    $('#header').html(" My Projects <br> <br>")
+});
+
+$('#btn4').on("click", function () {
+    $('#body').html('<style> #container{display: none;} </style>');
+    $('#body').html("<div class = 'row' <div class= 'col-12'><p> Github Profile: https://github.com/Developer994 </p></div> </div>");
+    $('#contact2').html("<div class = 'row' <div class= 'col-12'><p> Linkedin Profile: </p></div> </div>");
+    $('#contact3').html("<div class = 'row' <div class= 'col-12'><p> Email Address: rez_a@outlook.com </p></div> </div>");
+    $('#contact4').html("<div class = 'row' <div class= 'col-12'><p> Phone Number: (647) 608-0453 </p></div> </div>");
+    $('#header').html(" Contact <br> <br>")
+});
