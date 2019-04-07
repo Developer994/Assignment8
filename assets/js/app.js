6bc0e956e3662aa7ef90bc9efeea557b8070b774
Silent Dev <rez_a@outlook.com>
2019-04-06 23:32:57 -0400

    Fixed the issue where the contacts would remain on the page after you click on another sections button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,63 @@
 
+
+
+
+function website() {
+    $('#abtMeContainer').show();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+}
+website()
+
 $('#btn1').on("click", function () {
-    $('#body').html('<style> #container{display: none;} </style>');
-    $('#body').text('As a student in the field of full stack development, I am an individual who is compassionate in building websites and programming them. Other qualifications include an advanced diploma in Electrical Engineering Technology : Control Systems from Humber College and I also worked in construction.');
     $('#header').html("About Me <br> <br>");
+    $('#abtMeContainer').show();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
 });
 
 $('#btn2').on("click", function () {
-    $('#body').html('<style> #container{display: none;} </style>');
-    $('#body').text('I have learned and earned experience in html, css, javascript, jQuery, bootstrap, AJAX, JSON and with APIs. I have also create a project with my group to track interesting events in any cities that have events. This is available to be viewed down below in the project section.');
     $('#header').html(" My Experience <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').show();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
 });
 
 $('#btn3').on("click", function () {
-    $('#body').html('<style> #container{display: none;} </style>');
-    $('#body').text('I have learned and earned experience in html, css, javascript, jQuery, bootstrap, AJAX, JSON and with APIs. I have also create a project with my group to track interesting events in any cities that have events. This is available to be viewed down below in the project section.');
     $('#header').html(" My Projects <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').show();
+    $('#contactContainer').hide();
 });
 
 $('#btn4').on("click", function () {
-    $('#body').html('<style> #container{display: none;} </style>');
-    $('#body').html("<div class = 'row' <div class= 'col-12'><p> Github Profile: https://github.com/Developer994 </p></div> </div>");
-    $('#contact2').html("<div class = 'row' <div class= 'col-12'><p> Linkedin Profile: </p></div> </div>");
-    $('#contact3').html("<div class = 'row' <div class= 'col-12'><p> Email Address: rez_a@outlook.com </p></div> </div>");
-    $('#contact4').html("<div class = 'row' <div class= 'col-12'><p> Phone Number: (647) 608-0453 </p></div> </div>");
-    $('#header').html(" Contact <br> <br>")
+    $('#header').html(" My Projects <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').show();
 });
+// $('#btn2').on("click", function () {
+//     $('#body').html('<style> #container{display: none;} </style>');
+//     $('#body').text('I have learned and earned experience in html, css, javascript, jQuery, bootstrap, AJAX, JSON and with APIs. I have also create a project with my group to track interesting events in any cities that have events. This is available to be viewed down below in the project section.');
+//     $('#header').html(" My Experience <br> <br>")
+// });
+
+// $('#btn3').on("click", function () {
+//     $('#body').html('<style> #container{display: none;} </style>');
+//     $('#body').text('');
+//     $('#header').html(" My Projects <br> <br>")
+// });
+
+// $('#btn4').on("click", function () {
+//     $('#body').html('<style> #container{display: none;} </style>');
+//     $('#body').html("<div class = 'row' <div class= 'col-12'><p> Github Profile: https://github.com/Developer994 </p></div> </div>");
+//     $('#contact2').html("<div class = 'row' <div class= 'col-12'><p> Linkedin Profile: </p></div> </div>");
+//     $('#contact3').html("<div class = 'row' <div class= 'col-12'><p> Email Address: rez_a@outlook.com </p></div> </div>");
+//     $('#contact4').html("<div class = 'row' <div class= 'col-12'><p> Phone Number: (647) 608-0453 </p></div> </div>");
+//     $('#header').html(" Contact <br> <br>")
+// });
