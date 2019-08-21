3727eb7d2214e391bb4a44a444843b0a904c4aaf
Silent Dev <rez_a@outlook.com>
2019-08-21 12:55:46 -0400

    Fixed directory issue that was causing a 404 on the deployment

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+
+
+
+function website() {
+    $('#abtMeContainer').show();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+}
+website()
+
+$('#btn1').on("click", function () {
+    $('#header').html("About Me <br> <br>");
+    $('#abtMeContainer').show();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+});
+
+$('#btn2').on("click", function () {
+    $('#header').html(" My Experience <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').show();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+});
+
+$('#btn3').on("click", function () {
+    $('#header').html(" My Projects <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').show();
+    $('#contactContainer').hide();
+});
+
+$('#btn4').on("click", function () {
+    $('#header').html(" Contact <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').show();
+});
